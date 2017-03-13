eb2cd65969d205d497f53377ab653445e55a24d8
Razvan Florea <razvan_rv@yahoo.com>
2017-03-13 16:20:18 +0200

    initial commit

---
+++ src/main/java/com/example/SsoWithFacebookApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SsoWithFacebookApplication {
+
+    /**
+     * Single sign on app using facebook.
+     * Reference => https://spring.io/guides/tutorials/spring-boot-oauth2/
+     **/
+    public static void main(String[] args) {
+		SpringApplication.run(SsoWithFacebookApplication.class, args);
+	}
+}
